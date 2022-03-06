178fbf2c0bd8db906a60132aa4864a6d247b3982
McCayl <realcayl.cl@gmail.com>
2022-03-06 17:01:01 +0400

    added: controllers layer

---
+++ src/main/java/com/mccayl/infosystemweb/controller/AlbumController.java
@@ -0,0 +1,67 @@
+package com.mccayl.infosystemweb.controller;
+
+import com.mccayl.infosystemweb.model.Album;
+import com.mccayl.infosystemweb.model.Track;
+import com.mccayl.infosystemweb.service.impl.AlbumServiceImpl;
+import com.mccayl.infosystemweb.service.impl.TrackServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/albums")
+public class AlbumController {
+
+    @Autowired
+    private AlbumServiceImpl albumService;
+
+    @Autowired
+    private TrackServiceImpl trackService;
+
+    @GetMapping
+    public List<Album> getAll() {
+        return albumService.getAll();
+    }
+
+    @GetMapping("{id}")
+    public Album getAlbum(@PathVariable Long id) {
+        return albumService.getById(id);
+    }
+
+    @PostMapping
+    public Album addAlbum(@RequestBody Album album) {
+        return albumService.addAlbum(album);
+    }
+
+    @DeleteMapping("{id}")
+    public void delAlbum(@PathVariable Long id) {
+        albumService.deleteAlbumById(id);
+    }
+
+    @PutMapping("{id}")
+    public Album updateAlbum(@PathVariable Long id, @RequestBody Album album) {
+        return albumService.editAlbum(id, album);
+    }
+
+    @PostMapping("{id}/tracks")
+    public Track addTrack(@PathVariable Long id, @RequestBody Track track) {
+        return albumService.addTrackToAlbum(id, track);
+    }
+
+    @PostMapping("{id}/tracks/{trId}")
+    public Track addExstTrack(@PathVariable Long id, @PathVariable Long trId) {
+        return albumService.addTrackToAlbum(id, trackService.getById(trId));
+    }
+
+    @DeleteMapping("{id}/tracks/{trId}")
+    public void delTrack(@PathVariable Long id, @PathVariable Long trId) {
+        albumService.deleteTrackFromAlbum(id, trackService.getById(trId));
+    }
+
+    @GetMapping("{id}/tracks")
+    public List<Track> getTracks(@PathVariable Long id) {
+        return albumService.getAlbumTracks(id);
+    }
+
+}
---
+++ src/main/java/com/mccayl/infosystemweb/controller/TrackController.java
@@ -0,0 +1,42 @@
+package com.mccayl.infosystemweb.controller;
+
+import com.mccayl.infosystemweb.model.Track;
+import com.mccayl.infosystemweb.service.impl.TrackServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/tracks")
+public class TrackController {
+
+    @Autowired
+    private TrackServiceImpl trackService;
+
+    @GetMapping
+    public List<Track> getAll() {
+        return trackService.getAll();
+    }
+
+    @GetMapping("{id}")
+    public Track getTrack(@PathVariable Long id) {
+        return trackService.getById(id);
+    }
+
+    @PostMapping
+    public Track addTrack(@RequestBody Track track) {
+        return trackService.addTrack(track);
+    }
+
+    @DeleteMapping("{id}")
+    public void delTrack(@PathVariable Long id) {
+        trackService.deleteTrackById(id);
+    }
+
+    @PutMapping("{id}")
+    public Track updateTrack(@PathVariable Long id, @RequestBody Track track) {
+        return trackService.editTrack(id, track);
+    }
+
+}
